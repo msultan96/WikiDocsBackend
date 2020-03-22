c7d8fc31b3cba6bb2d140d32af40f4ee8dd976f9
muhammad.sultan01 <muhammad.sultan96@gmail.com>
2020-03-21 22:31:57 -0500

    Added email admin after submit and name articles on creation

--- src/main/java/com/infy/WikiDocsProject/Configuration/EtherPadConfig.java
+++ src/main/java/com/infy/WikiDocsProject/Configuration/EtherPadConfig.java
@@ -10,7 +10,7 @@ public class EtherPadConfig {
     @Bean
     public EPLiteClient epLiteClient(){
         /*
-            TODO: Change to read etherPadUrl and apiKey from application.properties
+            TODO: Change to read etherPadUrl and apiKey from cloud configuration
          */
         String etherPadUrl = "http://localhost:9001";
         String apiKey = "39745be536ed4fbdb3c805fe66d0b955a58681b34a4d6f1f710c52624104e311";
--- src/main/java/com/infy/WikiDocsProject/Repository/UserRepository.java
+++ src/main/java/com/infy/WikiDocsProject/Repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.infy.WikiDocsProject.Repository;
 import com.infy.WikiDocsProject.Model.User;
+import com.infy.WikiDocsProject.enums.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
+    List<User> findAllByRole(Role role);
 }
