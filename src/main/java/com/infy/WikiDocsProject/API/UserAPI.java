7317b85148f77c798e7aca92af15de5114a84d7a
muhammad.sultan01 <muhammad.sultan96@gmail.com>
2020-03-27 14:24:19 -0500

    Added authors to Article API methods

--- src/main/java/com/infy/WikiDocsProject/API/UserAPI.java
+++ src/main/java/com/infy/WikiDocsProject/API/UserAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * 
  * User API Controller Class
- *
+ * @author Muhammad Sultan
  */
 @SuppressWarnings("UnnecessaryLocalVariable")
 @RestController
--- src/main/java/com/infy/WikiDocsProject/Service/ArticleServiceImpl.java
+++ src/main/java/com/infy/WikiDocsProject/Service/ArticleServiceImpl.java
@@ -12,6 +12,8 @@ import com.infy.WikiDocsProject.enums.Status;
 import java.util.*;
 
 import net.gjerull.etherpad.client.EPLiteClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,7 +22,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 /**
@@ -36,6 +37,8 @@ public class ArticleServiceImpl implements ArticleService {
 	private final UserRepository userRepository;
 	private final RoleRepository roleRepository;
 	private final JavaMailSender javaMailSender;
+
+	static Logger logger = LogManager.getLogger(ArticleServiceImpl.class);
 
 	/**
 	 * Constructor using constructor injection
@@ -62,6 +65,7 @@ public class ArticleServiceImpl implements ArticleService {
 //		return articles;
 //	}
 
+
 	/**
 	 * Retrieves the list of articles of a user
 	 *
@@ -69,11 +73,17 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return The list of articles of a user
 	 */
 	public List<Article> getAllArticlesByEmailId(String email, int pageNumber, int pageSize) {
+		logger.info("Getting all articles by email " + email);
+		email = email.toLowerCase();
+		logger.info("Validating email exists in database");
 		customUserDetailsService.findByEmail(email);
 
+		logger.info("Requesting database for articles on page "+ pageNumber + " with page size " + pageSize);
 		Pageable pageWithSize = PageRequest.of(pageNumber, pageSize);
 		Page<Article> page = articleRepository.findAllArticlesByEmailId(email, pageWithSize);
 		List<Article> articles = page.getContent();
+
+		logger.info("Retrieved " + articles.size() + " articles");
 		return articles;
 	}
 
@@ -82,9 +92,14 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return the list of approved articles
 	 */
 	public List<Article> getAllArticlesByStatus(Status status, int pageNumber, int pageSize){
+		logger.info("Getting all articles of status " + status.toString());
+
+		logger.info("Requesting database for articles on page "+ pageNumber + " with page size " + pageSize);
 		Pageable pageWithSize = PageRequest.of(pageNumber, pageSize);
 		Page<Article> page = articleRepository.findArticlesByStatus(status, pageWithSize);
 		List<Article> articles = page.getContent();
+
+		logger.info("Retrieved " + articles.size() + " articles");
 		return articles;
 	}
 
@@ -98,27 +113,35 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @throws UserNotFoundException If the email isn't found
 	 */
 	public List<Article> getAllArticlesByEmailIdAndStatus(String email, Status status, int pageNumber, int pageSize) {
+		logger.info("Getting all articles by email " + email + " with status " + status.toString());
+		email = email.toLowerCase();
 		customUserDetailsService.findByEmail(email);
 
+		logger.info("Requesting database for articles on page "+ pageNumber + " with page size " + pageSize);
 		Pageable pageWithSize = PageRequest.of(pageNumber, pageSize);
 		Page<Article> page = articleRepository.findAllArticlesByEmailIdAndStatus(email, status, pageWithSize);
 		List<Article> articles = page.getContent();
+
+		logger.info("Retrieved " + articles.size() + " articles");
 		return articles;
 	}
 
 	/**
 	 * Retrieve an article with a specific id.
+	 * @author Luong Dinh
 	 * @param id Used to locate the article
 	 * @return the article found
 	 * @throws ArticleNotFoundException If the id isn't associated with an article
 	 */
 	public Article findById(ObjectId id){
+		logger.info("Finding article with id " + id.toHexString());
 		// Call findById from articleRepository to find the article with the given id
 		Optional<Article> optionalArticle = articleRepository.findById(id);
 
 		// If the article is present, then return it
 		// otherwise, throw an Article Not Found Exception
 		if(optionalArticle.isPresent()){
+			logger.info("Article found");
 			return optionalArticle.get();
 		}
 		else{
@@ -128,9 +151,12 @@ public class ArticleServiceImpl implements ArticleService {
 
 	/**
 	 * Overloaded method using ObjectId as parameter instead
+	 * to convert from String to ObjectId
+	 * @author Luong Dinh
 	 * @see ArticleServiceImpl#findById(ObjectId)
 	 */
 	public Article findById(String id){
+		logger.info("Converting string id to ObjectId");
 		// Convert the string to an objectId
 		ObjectId objectId = new ObjectId(id);
 
@@ -148,11 +174,13 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return the updated Article
 	 */
 	public Article submitArticle(String id) {
+		logger.info("Submitting article with id " + id);
 		// Call findById to retrieve the article and validate that it exists
 		Article article = findById(id);
 
 		// A submitting article can't be of status DISCARDED or APPROVED
 		// throw appropriate exception
+		logger.info("Verifying article status meets requirements");
 		if (article.getStatus() == Status.DISCARDED) {
 			throw new SubmittingArticleIsDiscardedException("ArticleService.SUBMITTING_ARTICLE_DISCARDED");
 		} else if (article.getStatus() == Status.APPROVED) {
@@ -163,10 +191,15 @@ public class ArticleServiceImpl implements ArticleService {
 		else{
 			// If the article is of status INITIAL or REJECTED
 			// Set the status to BETA
+			logger.info("Changing article status to beta");
 			article.setStatus(Status.BETA);
-			/*
-			TODO: Send an email to Administrator
-			 */
+
+			logger.info("Saving article");
+			// Save the article
+			articleRepository.save(article);
+
+			logger.info("Locating all admins for broadcast email");
+			// Find all admins in order to send them an email
 			Role adminRole = roleRepository.findByRole("ADMIN");
 			List<User> users = userRepository.findAll();
 			List<User> admins = new ArrayList<>();
@@ -177,11 +210,15 @@ public class ArticleServiceImpl implements ArticleService {
 					admins.add(user);
 				}
 			});
+			logger.info("Located " + admins.size() + " admins");
 
+			logger.info("Setting up email list");
 			String[] adminEmailsArray = new String[admins.size()];
-			for(int i=0; i<admins.size(); i++){
+			for(int i=0; i<admins.size(); i++) {
 				adminEmailsArray[i] = admins.get(i).getEmail();
 			}
+
+			logger.info("Creating email to admins");
 			SimpleMailMessage email = new SimpleMailMessage();
 			email.setTo(adminEmailsArray);
 			email.setSubject("Article pending approval");
@@ -192,10 +229,11 @@ public class ArticleServiceImpl implements ArticleService {
 //					+ "Reject: " + "http://localhost:8080/DLM_Wiki/ArticleAPI/rejectArticle/" + article.getId().toHexString();
 					+ "Head on over to DLM Wiki to make further action " + "http://localhost:4200";
 			email.setText(emailBody);
-			javaMailSender.send(email);
 
-			// Save the article
-			articleRepository.save(article);
+			logger.info("Sending email");
+			javaMailSender.send(email);
+			logger.info("Email successfully sent");
+
 			// return the article with the new status
 			return article;
 		}
@@ -210,11 +248,13 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return the updated Article
 	 */
 	public Article approveArticle(String id){
+		logger.info("Approving article with id " + id);
 		// Call findById to retrieve the article and validate that it exists
 		Article article = findById(id);
 
 		// An article being approved can only be of status BETA
 		// throw appropriate exceptions otherwise
+		logger.info("Verifying article status meets requirements");
 		if(article.getStatus() == Status.REJECTED){
 			throw new ApprovingArticleIsStillRejectedException("ArticleService.APPROVING_ARTICLE_REJECTED");
 		} else if(article.getStatus() == Status.INITIAL){
@@ -226,10 +266,14 @@ public class ArticleServiceImpl implements ArticleService {
 		}
 		else{
 			// Set the status to APPROVED
+			logger.info("Changing article status to approved");
 			article.setStatus(Status.APPROVED);
+
 			// Save the article
+			logger.info("Saving article");
 			articleRepository.save(article);
 			// return the article with the new status
+			logger.info("Article successfully submitted");
 			return article;
 		}
 
@@ -244,11 +288,13 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return the updated Article
 	 */
 	public Article rejectArticle(String id){
+		logger.info("Rejecting article with id " + id);
 		// Call findById to retrieve the article and validate that it exists
 		Article article = findById(id);
 
 		// An article being rejected can only be of status BETA
 		// throw appropriate exceptions otherwise
+		logger.info("Verifying article status meets requirements");
 		if(article.getStatus() == Status.REJECTED){
 			throw new RejectingArticleIsStillRejectedException("ArticleService.REJECTING_ARTICLE_REJECTED");
 		} else if(article.getStatus() == Status.INITIAL){
@@ -259,13 +305,16 @@ public class ArticleServiceImpl implements ArticleService {
 			throw new RejectingArticleIsDiscardedException("ArticleService.REJECTING_ARTICLE_DISCARDED");
 		}
 		else{
+			logger.info("Rejecting article");
 			// Increase the rejection count of the article
 			article.setRejectedCount(article.getRejectedCount() + 1);
+
 			// If it's been allowed more than 3 edits,
 			// then change status to DISCARDED
 			// otherwise change the status to REJECTED
 			if (article.getRejectedCount() >= 4) article.setStatus(Status.DISCARDED);
 			else article.setStatus(Status.REJECTED);
+			logger.info("Article is now " + article.getStatus().toString());
 			// Save the article
 			articleRepository.save(article);
 			// return the article with the new status
@@ -280,6 +329,8 @@ public class ArticleServiceImpl implements ArticleService {
 	 * @return article The new article created
 	 */
 	public Article createArticleByEmail(String email, String articleName){
+		logger.info("Creating new article '" + articleName + "' for email " + email);
+		email = email.toLowerCase();
 		// Validate that the user with the given email exists
 		// and retrieve the user
 		User user = customUserDetailsService.findByEmail(email);
@@ -290,6 +341,7 @@ public class ArticleServiceImpl implements ArticleService {
 
 		// Declared new article object and
 		// set with with the article builder with initial parameters
+		logger.info("Building article");
 		Article newArticle = Article.builder()
 				.id(new ObjectId())
 				.emailId(user.getEmail())
@@ -298,57 +350,73 @@ public class ArticleServiceImpl implements ArticleService {
 				.readOnly(false)
 				.build();
 
+		logger.info("Adding article to user");
 		// add the new article to the list of articles
 		articles.add(newArticle);
 
 		// set the articles list to the users articles
 		user.setArticles(articles);
 
+		logger.info("Creating EtherPad with article id " + newArticle.getId().toString());
 		//create an etherPad with the article's ObjectId
 		epLiteClient.createPad(newArticle.getId().toString());
 
+		logger.info("Saving article");
 		// save article to database
 		articleRepository.save(newArticle);
 
+		logger.info("Saving user");
 		// save user to database
 		userRepository.save(user);
 
 		// return new article object
+		logger.info("Article created");
 		return newArticle;
 	}
 
 	/**
 	 * Save an article with the given id
-	 *
+	 * @author Luong Dinh
 	 * @param etherPadId id of the ether pad and article
 	 * @return article The saved article
 	 */
 	public Article saveArticle(String etherPadId) {
+		logger.info("Saving article with id " + etherPadId);
 		// Call findById to validate that the article does exist
 		Article article = findById(etherPadId);
+
+		logger.info("Verifying article status meets requirements");
 		switch(article.getStatus()){
 			case APPROVED:
 			case BETA:
 			case DISCARDED:
 				throw new SavingArticleIsSubmittedException("ArticleService.EDITING_ARTICLE_SUBMITTED");
 		}
+		logger.info("Retrieving EtherPad content");
 		// Retrieve the contents of the ether pad
 		String content = epLiteClient.getText(etherPadId).get("text").toString();
 		if(content == null) content = "";
+		logger.info("Setting EtherPad content");
 		epLiteClient.setText(etherPadId, content);
 		// Set the article's contents to the ether pad content
+		logger.info("Setting article content to EtherPad content");
 		article.setContent(content);
 		// Save the article with updated content
+		logger.info("Saving article in database");
 		articleRepository.save(article);
+		logger.info("Saved article");
 		return article;
 	}
 
 	/**
 	 * Get the appropriate URL for the ether pad requested
+	 * @author Luong Dinh
 	 * @param id the id of the article
 	 * @return ether pad url
 	 */
 	public String getEtherPadUrl(String id){
+		logger.info("Retrieving Ether Pad Url");
+
 		// Call findById to validate that the article does exist
 		Article article = findById(id);
 
@@ -361,33 +429,43 @@ public class ArticleServiceImpl implements ArticleService {
 			case APPROVED:
 			case BETA:
 			case DISCARDED:
+				logger.info("Setting up read only id");
 				appendingId = epLiteClient.getReadOnlyID(id).get("readOnlyID").toString() + "?";
 				appendingId = appendingId +  "showControls=false";
+				logger.info("Setup complete");
 				break;
 			case INITIAL:
 			case REJECTED:
+				logger.info("Setting up editable id");
 				appendingId = id + "?";
+				logger.info("Setup complete");
 		}
 		etherPadUrl = etherPadUrl + appendingId;
+		logger.info("Setting up EtherPad with article content");
 		if(article.getContent() == null) article.setContent("");
 		epLiteClient.setText(id,article.getContent());
+		logger.info("EtherPad URL retrieved");
 		return etherPadUrl;
 	}
 
 	/**
 	 * Get All Articles a User has been invited to collaborate on
+	 * @author Muhammad Sultan
 	 * @param email email of the user
 	 * @param pageNumber current page number
 	 * @param pageSize size of the page
 	 * @return the collaborating articles
 	 */
 	public List<Article> getAllInvitedArticlesByEmail(String email, int pageNumber, int pageSize){
+		logger.info("Retrieving all collaborating articles for " + email);
+		email = email.toLowerCase();
 		List<Article> articles = new ArrayList<>();
 
 		//Retrieve the user + their collaborating articles
 		User user = customUserDetailsService.findByEmail(email);
 		List<ObjectId> collaboratingArticlesById = user.getCollaboratingArticles();
 
+		logger.info("Removing articles that have had their status changed");
 		//Remove any articles that aren't editable
 		collaboratingArticlesById.removeIf(id -> {
 			Article article = findById(id);
@@ -401,28 +479,43 @@ public class ArticleServiceImpl implements ArticleService {
 			}
 		});
 
+		logger.info("Saving user with new collaborating articles");
 		user.setCollaboratingArticles(collaboratingArticlesById);
 		userRepository.save(user);
 
 		//Convert the list into pages
+		logger.info("Requesting database for collaborating articles of page "+ pageNumber + " with page size " + pageSize);
 		Pageable pageable = PageRequest.of(pageNumber, pageSize);
 		int start = Math.toIntExact(pageable.getOffset());
 		int end = Math.min((start + pageable.getPageSize()), articles.size());
 
 		if(start>end) return Collections.emptyList();
 		Page<Article> page = new PageImpl<>(articles.subList(start, end), pageable, articles.size());
+		List<Article> articlesInPage = page.getContent();
 
+		logger.info("Retrieved " + articlesInPage.size() + " articles");
 		//return the current page
-		return page.getContent();
+		return articlesInPage;
 	}
 
+	/**
+	 * Add a user to collaborate on a given article
+	 * @author Muhammad Sultan
+	 * @param map
+	 * @return
+	 */
 	public String inviteUserToCollaborateByEmail(Map<String, String> map){
-		String inviteeEmail = map.get("email");
+
+		String inviteeEmail = map.get("email").toLowerCase();
 		String articleIdAsString = map.get("articleId");
 		ObjectId articleId = new ObjectId(articleIdAsString);
+		logger.info("Inviting user to collaborate with email " + inviteeEmail + " on article " + articleIdAsString);
 
 		//validate the user email exists
 		User invitee = customUserDetailsService.findByEmail(inviteeEmail);
+
+		//Validate article id
+		findById(articleId);
 
 		//retrieve the articles to mutate later
 		List<ObjectId> inviteeArticles = invitee.getCollaboratingArticles();
@@ -431,15 +524,15 @@ public class ArticleServiceImpl implements ArticleService {
 			throw new UserAlreadyInvitedException("ArticleService.USER_ALREADY_INVITED");
 		}
 
-		//Validate article id
-		findById(articleId);
-
+		logger.info("Updating users collaborating articles");
 		//Add to users collaborating articles
 		inviteeArticles.add(articleId);
 
+		logger.info("Saving user");
 		//Save user with modified collaborating articles
 		userRepository.save(invitee);
 
+		logger.info(inviteeEmail + " successfully invited to collaborate");
 		return invitee.getName();
 	}
 
