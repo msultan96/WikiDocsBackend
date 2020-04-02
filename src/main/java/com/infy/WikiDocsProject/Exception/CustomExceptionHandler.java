2a8524340887c477139c8ad217c2a9ffd730676b
Daniel Neal <danielneal@verybluemoon.net>
2020-04-02 16:01:59 -0500

    Added AOP and ExceptionHandler Comments

--- src/main/java/com/infy/WikiDocsProject/Exception/CustomExceptionHandler.java
+++ src/main/java/com/infy/WikiDocsProject/Exception/CustomExceptionHandler.java
@@ -9,113 +9,186 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import lombok.extern.slf4j.Slf4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static org.springframework.http.HttpStatus.*;
 
-@Slf4j
+/**
+ * CustomExceptionHandler to handle all RuntimeExceptions
+ * @author: Daniel Neal
+ */
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
 
     @Autowired
     private final Environment environment;
 
+    static Logger log = LogManager.getLogger(CustomExceptionHandler.class);
+
+    /**
+     * Constructor
+     * @param environment
+     */
     public CustomExceptionHandler(Environment environment) {
         this.environment = environment;
     }
 
+    /**
+     * ExceptionHandler for the ApprovingArticleIsApprovedException
+     */
     @ExceptionHandler({ApprovingArticleIsApprovedException.class})
     public ResponseEntity<ErrorMessage> approvingArticleIsApprovedException(ApprovingArticleIsApprovedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the ApprovingArticleIsDiscardedException
+     */
     @ExceptionHandler({ApprovingArticleIsDiscardedException.class})
     public ResponseEntity<ErrorMessage> approvingArticleIsDiscardedException(ApprovingArticleIsDiscardedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the ApprovingArticleIsInitialException
+     */
     @ExceptionHandler({ApprovingArticleIsInitialException.class})
     public ResponseEntity<ErrorMessage> approvingArticleIsInitialException(ApprovingArticleIsInitialException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the ApprovingArticleIsStillRejectedException
+     */
     @ExceptionHandler({ApprovingArticleIsStillRejectedException.class})
     public ResponseEntity<ErrorMessage> approvingArticleIsStillRejectedException(ApprovingArticleIsStillRejectedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the RejectingArticleIsApprovedException
+     */
     @ExceptionHandler({RejectingArticleIsApprovedException.class})
     public ResponseEntity<ErrorMessage> rejectingArticleIsApprovedException(RejectingArticleIsApprovedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the RejectingArticleIsDiscardedException
+     */
     @ExceptionHandler({RejectingArticleIsDiscardedException.class})
     public ResponseEntity<ErrorMessage> rejectingArticleIsDiscardedException(RejectingArticleIsDiscardedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the RejectingArticleIsInitialException
+     */
     @ExceptionHandler({RejectingArticleIsInitialException.class})
     public ResponseEntity<ErrorMessage> rejectingArticleIsInitialException(RejectingArticleIsInitialException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the RejectingArticleIsStillRejectedException
+     */
     @ExceptionHandler({RejectingArticleIsStillRejectedException.class})
     public ResponseEntity<ErrorMessage> rejectingArticleIsStillRejectedException(RejectingArticleIsStillRejectedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the SubmittingArticleIsApprovedException
+     */
     @ExceptionHandler({SubmittingArticleIsApprovedException.class})
     public ResponseEntity<ErrorMessage> submittingArticleIsApprovedException(SubmittingArticleIsApprovedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the SubmittingArticleIsBetaException
+     */
     @ExceptionHandler({SubmittingArticleIsBetaException.class})
     public ResponseEntity<ErrorMessage> submittingArticleIsBetaException(SubmittingArticleIsBetaException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the SubmittingArticleIsDiscardedException
+     */
     @ExceptionHandler({SubmittingArticleIsDiscardedException.class})
     public ResponseEntity<ErrorMessage> submittingArticleIsDiscardedException(SubmittingArticleIsDiscardedException e) {
         return error(BAD_REQUEST, e);
     }
 
+    /**
+     * ExceptionHandler for the ArticleNotFoundException
+     */
     @ExceptionHandler({ArticleNotFoundException.class})
     public ResponseEntity<ErrorMessage> articleNotFoundException(ArticleNotFoundException e) {
         return error(NOT_FOUND, e);
     }
 
+    /**
+     * ExceptionHandler for the UserNotFoundException
+     */
     @ExceptionHandler({UserNotFoundException.class})
     public ResponseEntity<ErrorMessage> userNotFoundException(UserNotFoundException e) {
         return error(NOT_FOUND, e);
     }
 
+    /**
+     * ExceptionHandler for the UserAlreadyExistsException
+     */
     @ExceptionHandler({UserAlreadyExistsException.class})
     public ResponseEntity<ErrorMessage> userAlreadyExistsException(UserAlreadyExistsException e){
         return error(CONFLICT, e);
     }
 
+    /**
+     * ExceptionHandler for the IncorrectCredentialsException
+     */
     @ExceptionHandler({IncorrectCredentialsException.class})
     public ResponseEntity<ErrorMessage> incorrectCredentialsException(IncorrectCredentialsException e) {
         return error(UNAUTHORIZED, e);
     }
 
+    /**
+     * ExceptionHandler for the {@link UserAlreadyInvitedException}
+     */
     @ExceptionHandler({UserAlreadyInvitedException.class})
     public ResponseEntity<ErrorMessage> userAlreadyInvitedException(UserAlreadyInvitedException e) {
         return error(CONFLICT, e);
     }
 
+    /**
+     * ExceptionHandler for the {@link UsernameNotFoundException}
+     */
     @ExceptionHandler({UsernameNotFoundException.class})
     public ResponseEntity<ErrorMessage> UsernameNotFoundException(UsernameNotFoundException e) {
         return error(UNAUTHORIZED, e);
     }
 
+    /**
+     * ExceptionHandler for all other {@link RuntimeException}
+     */
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<ErrorMessage> exception(RuntimeException e) {
         return error(INTERNAL_SERVER_ERROR, e);
     }
 
+    /**
+     * error, logs any exceptions that have ocurred
+     * @param status
+     * @param e
+     * @return new ResponseEntity<>(error,status)
+     */
     private ResponseEntity<ErrorMessage> error(HttpStatus status, Exception e) {
-//        log.error("The following exception occurred during processing:  ", e);
-//        log.info(environment.getProperty(e.getMessage()));
+        logger.error("The following exception occurred during processing:  ", e);
+        logger.info(environment.getProperty(e.getMessage()));
         ErrorMessage error = new ErrorMessage(status.value(), environment.getProperty(e.getMessage()));
         return new ResponseEntity<>(error, status);
     }
--- src/main/java/com/infy/WikiDocsProject/Utility/LoggingAspect.java
+++ src/main/java/com/infy/WikiDocsProject/Utility/LoggingAspect.java
@@ -13,22 +13,38 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+
+/**
+ * Spring AOP LoggingAspect Class using Log4J
+ */
 @Aspect
 @Component
-public class LoggingAdvice {
+public class LoggingAspect {
 
-    static Logger log = LogManager.getLogger(LoggingAdvice.class);
+    static Logger log = LogManager.getLogger(LoggingAspect.class);
 
+    /**
+     * Pointcut value for all the API classes
+     */
     @Pointcut(value="execution(* com.infy.WikiDocsProject.API.*.*(..))")
     public void apiPointcut(){
 
     }
 
+    /**
+     * Pointcut value for all the Service Implementation classes
+     */
     @Pointcut(value="execution(* com.infy.WikiDocsProject.Service.*.*(..))")
     public void servicePointcut(){
 
     }
 
+    /**
+     * @Around advice used for generic logging around all pointcuts
+     * @param proceedingJoinPoint
+     * @return Object
+     * @throws Throwable
+     */
     @Around("(apiPointcut() || servicePointcut())")
     public Object applicationLogger(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -37,46 +53,71 @@ public class LoggingAdvice {
         String methodName = proceedingJoinPoint.getSignature().getName();
         Object[] argsObject = proceedingJoinPoint.getArgs();
 
+        //masks sensitive login/registration info
         handleRawPassword(methodName, argsObject);
 
+        //logs generic info before pointcut/method execution
         log.info("\n" + className + "\n\t" + methodName +"\n(" + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(argsObject) + ")" +
                 "\n\t" + "invoked");
 
         final StopWatch stopWatch = new StopWatch();
         stopWatch.start();
+
+        //executes the method
         Object response = proceedingJoinPoint.proceed();
         stopWatch.stop();
 
+        //logs generic info, including the report time, after the poincut/method execution
         log.info("\n" + className + "\n\t" + methodName +"\n(" + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(argsObject) + ")" +
                 "\n\t" + "took " + stopWatch.getTotalTimeMillis() + " ms" +
                 "\n\t" + "response: \n" + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
         return response;
     }
 
+    /**
+     * handleRawPassword used to mask sensitive login/registration info
+     * @param methodName
+     * @param argsObject
+     * @throws CloneNotSupportedException
+     */
     private void handleRawPassword(String methodName, Object[] argsObject) throws CloneNotSupportedException {
         if(methodName.equals("loginUser")){
+            //clones argsObject so that actual value of argsObject is not changed
             AuthBody authBody = ((AuthBody)argsObject[0]).clone();
             authBody.setPassword("**********");
             argsObject[0] = authBody;
         }
 
         if(methodName.equals("register")){
+            //clones argsObject so that actual value of argsObject is not changed
             User user = ((User)argsObject[0]).clone();
             user.setPassword("**********");
             argsObject[0] = user;
         }
     }
 
+    /**
+     * @AfterThrowing advice used to log exceptions that occured during the execution of Service classes
+     * @param exception
+     */
     @AfterThrowing(pointcut="servicePointcut()", throwing="exception")
     public void logExceptionFromService(Exception exception){
         exceptionLogger(exception);
     }
 
+    /**
+     * @AfterThrowing advice used to log exceptions that occured during the execution of API classes
+     * @param exception
+     */
     @AfterThrowing(pointcut="apiPointcut()", throwing="exception")
     public void logExceptionFromAPI(Exception exception){
         exceptionLogger(exception);
     }
 
+    /**
+     * exceptionLogger, logs any exceptions that occurred during the execution of the application
+     * @param exception
+     */
     private void exceptionLogger(Exception exception){
         if(exception.getMessage()!=null &&
                 (exception.getMessage().contains("Service") || exception.getMessage().contains("API"))){
